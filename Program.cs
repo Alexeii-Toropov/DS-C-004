a5a97b83e013a5b1031b71d9af4403b0a409a9c8
Alexeii Toropov <leha.toropov.04@mail.ru>
2022-08-19 14:55:02 +0300

    Finish

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int Pow(int number1, int number2)
+{
+  int res = 1;
+  for(int i=1; i <= number2; i++)
+  {
+    res = res * number1;
+  }
+  return res;
+}
+  int ResPow = Pow(number1, number2);
+  Console.WriteLine("Ответ: " + ResPow);
