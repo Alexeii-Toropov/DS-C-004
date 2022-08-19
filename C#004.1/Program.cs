72cca927a7172e439b54f22f92dd64ceda6c924e
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-19 15:55:46 +0300

    Add files via upload

---
+++ C#004.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите произвольное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int number)
+{
+    int length = Convert.ToString(number).Length;
+    int j = 0;
+    int res = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+      j = number - number % 10;
+      res = res + (number - j);
+      number = number / 10;
+    }
+    return res;
+}
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр: " + sumNumber);
